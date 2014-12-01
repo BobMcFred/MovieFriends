28a48535c84676314ebc9fcb39c3e957ce65d39d
Justin Turner <jturne58@uwo.ca>
2014-12-01 06:57:28 -0500

    additional work

--- app/routes.js
+++ app/routes.js
@@ -21,9 +21,6 @@ module.exports = function(app, passport, fs, Movie, userMovie) {
         failureFlash : true // allow flash messages
     }));
 
-    // =====================================
-    // SIGNUP ==============================
-    // =====================================
     // show the signup form
     app.get('/signup', function(req, res) {
 
@@ -38,31 +35,21 @@ module.exports = function(app, passport, fs, Movie, userMovie) {
         failureFlash : true // allow flash messages
     }));
 
-    // =====================================
-    // PROFILE SECTION =====================
-    // =====================================
-    // we will want this protected so you have to be logged in to visit
-    // we will use route middleware to verify this (the isLoggedIn function)
+    // Profile section - this was implemented before angular, and doesn't get used
     app.get('/profile', isLoggedIn, function(req, res) {
         res.render('profile.ejs', {
             user : req.user // get the user out of session and pass to template
         });
     });
     
-    // =====================================
-    // HOME SECTION ========================
-    // =====================================
-    // we will want this protected so you have to be logged in to visit
-    // we will use route middleware to verify this (the isLoggedIn function)
+    // home section - this was implemented before angular, and doesn't get used
     app.get('/home', isLoggedIn, function(req, res) {
         res.render('home.ejs', {
             user : req.user // get the user out of session and pass to template
         });
     });
 
-    // =====================================
-    // LOGOUT ==============================
-    // =====================================
+    // logout
     app.get('/logout', function(req, res) {
         req.logout();
         res.redirect('/');
@@ -163,10 +150,10 @@ module.exports = function(app, passport, fs, Movie, userMovie) {
 // route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
 
-    // if user is authenticated in the session, carry on 
+    // check if user is authenticated in the session
     if (req.isAuthenticated())
         return next();
 
-    // if they aren't redirect them to the home page
+    // redirect to home if not in session
     res.redirect('/');
 }
